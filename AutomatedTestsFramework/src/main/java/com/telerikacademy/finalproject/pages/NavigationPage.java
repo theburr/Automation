612cbf188ea1fd31dc862b8d9c4728fb7129a7d8
icokostow@gmail.com <Magdalenna78>
2020-11-17 20:50:02 +0200

    "Made changes in navigation page"

--- AutomatedTestsFramework/src/main/java/com/telerikacademy/finalproject/pages/NavigationPage.java
+++ AutomatedTestsFramework/src/main/java/com/telerikacademy/finalproject/pages/NavigationPage.java
@@ -16,6 +16,8 @@ public class NavigationPage extends BasePage implements Elements {
         super("base.url");
     }
 
+    // It is for delete //
+
     public void admin_LogIn() {
         NavigationPage navPage = new NavigationPage();
         actions.isElementPresentUntilTimeout(navPage.signInButton, 4);
